acf024d4352e820af822b79d0f28370a659a72ea
trevorwinser <64448847+trevorwinser@users.noreply.github.com>
2024-06-22 15:09:04 -0400

    Initial Version
    - Movement keys work
    - Animations work
    - Need to fix switching to plague doctor

--- characters/mc/mc.gd
+++ characters/mc/mc.gd
@@ -1,53 +1,66 @@
 extends CharacterBody2D
 
-@export var move_speed : int = 32
-@export var starting_direction: Vector2 = Vector2(0, -0.1)
-@export var is_priest : bool = true
+@export var move_speed : int = 16
+@export var starting_direction: Vector2 = Vector2(0, 1)
+@export var is_priest: bool = true
 
-#@onready var animation_tree = $AnimationTree
-#@onready var state_machine = animation_tree.get("parameters/playback")
+@onready var animation_tree = $AnimationTree
+@onready var character_state = animation_tree.get("parameters/playback")
+@onready var priest_state = animation_tree.get("parameters/Priest/playback")
+@onready var plague_state = animation_tree.get("parameters/Plague/playback")
+var input_direction = Vector2.ZERO
 
-#func _ready():
-	#update_animation_parameters(starting_direction)
+func _ready():
+	update_animation_parameters(starting_direction)
 
 func _physics_process(_delta):
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
-	
-	#update_animation_parameters(input_direction)
-	
-	velocity = input_direction.normalized() * move_speed
-	
+	if (input_direction != Vector2.ZERO):
+		print(input_direction)
+	velocity = input_direction * move_speed
+
 	move_and_slide()
 	
-	#pick_new_state()
-	#
-#
-#func update_animation_parameters(move_input : Vector2):
-	#if (move_input != Vector2.ZERO):
-			#animation_tree.set("parameters/Walk_Priest/BlendSpace2D/blend_position", move_input)
-			#animation_tree.set("parameters/Idle_Priest/BlendSpace2D/blend_position", move_input)
-			#animation_tree.set("parameters/Walk_Plague/BlendSpace2D/blend_position", move_input)
-			#animation_tree.set("parameters/Idle_Plague/BlendSpace2D/blend_position", move_input)
-#
-#func pick_new_state():
-	#var state = ""
-	#if (velocity != Vector2.ZERO):
-		#state += "Walk_"
-		#if (is_priest):
-			#state += "Priest"
-		#else:
-			#state += "Plague"
+	update_animation_parameters(input_direction)
+	pick_new_state()
+
+
+func update_animation_parameters(move_input : Vector2):
+	if (move_input != Vector2.ZERO):
+			animation_tree.set("parameters/Priest/Walk/blend_position", move_input)
+			animation_tree.set("parameters/Priest/Idle/blend_position", move_input)
+			animation_tree.set("parameters/Plague/Walk/bledsnd_position", move_input)
+			animation_tree.set("parameters/Plague/Idle/blend_position", move_input)
+
+func pick_new_state():
+	var state = ""
+	if (velocity != Vector2.ZERO):
+		state += "Walk"
+	else:
+		state += "Idle"
+	if (is_priest):
+		priest_state.travel(state)
 	#else:
-		#state += "Idle_"
-		#if (is_priest):
-			#state += "Priest"
-		#else:
-			#state += "Plague"
-	#state_machine.travel(state)
-		#
-#func _input(event):
-	#if event.is_action_pressed("switch"):
-		#is_priest = !is_priest
+		#plague_state.travel(state)
+	
+func _input(event):
+	if event.is_action_pressed("switch"):
+		is_priest = !is_priest
+		if (is_priest):
+			character_state.travel("Priest")
+		else:
+			character_state.travel("Plague")
+	elif event.is_action_pressed("exit"):
+			get_tree().quit()
+	elif event.is_action_pressed("up"):
+		input_direction = Vector2(0,-1)
+	elif event.is_action_pressed("down"):
+		input_direction = Vector2(0,1)
+	elif event.is_action_pressed("left"):
+		input_direction = Vector2(-1,0)
+	elif event.is_action_pressed("right"):
+		input_direction = Vector2(1,0)
+		
